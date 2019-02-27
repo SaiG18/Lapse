451ec74a99057f97ee0dac3a684c7c2a7799ae4d
SaiG18 <saigurrapu18@gmail.com>
2019-02-27 13:07:53 -0500

    Intial Commit

---
+++ progress.1h.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Progress Bar of Day, Week, Month, Year, and Life
+#
+# by Sai G (http://github.com/SaiG18)
+#
+# <bitbar.title>Lapse</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>SaiG18</bitbar.author>
+# <bitbar.author.github>SaiG18</bitbar.author.github>
+# <bitbar.desc>Progress Bar of Day, Week, Month, Year, and Life.</bitbar.desc>
+# <bitbar.dependencies>bash</bitbar.dependencies>
+
